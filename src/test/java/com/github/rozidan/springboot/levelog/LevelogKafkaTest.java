11b83c25c4ab09214c2a42f958fbb9a8b6dcbf91
Idan Roz <rozidan@gmail.com>
2017-08-25 17:54:22 +0300

    more test coverage

--- src/test/java/com/github/rozidan/springboot/levelog/LevelogKafkaTest.java
+++ src/test/java/com/github/rozidan/springboot/levelog/LevelogKafkaTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2017 Idan Rozenfeld the original author or authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -72,10 +72,11 @@ public class LevelogKafkaTest {
         levelogProvider.changeLogLevel(LogLevel.INFO, LevelogKafkaTest.class.getName());
         assertTrue(log.isInfoEnabled());
 
-        kafkaTemplate.send(TOPIC, Message.builder()
-                .logLevel(LogLevel.ERROR)
-                .loggerName(LevelogKafkaTest.class.getName())
-                .build());
+        Message msg = new Message();
+        msg.setLogLevel(LogLevel.ERROR);
+        msg.setLoggerName(LevelogKafkaTest.class.getName());
+
+        kafkaTemplate.send(TOPIC, msg);
         assertThat(listener.countDownLatch.await(10, TimeUnit.SECONDS)).isTrue();
 
         TimeUnit.SECONDS.sleep(1);
