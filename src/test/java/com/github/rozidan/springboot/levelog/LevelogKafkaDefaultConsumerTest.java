f8fba4a1d10c00f39452fffe95fd5ee98a817ac5
Idan Roz <rozidan@gmail.com>
2017-09-06 22:53:32 +0300

    kafka consumer config

--- src/test/java/com/github/rozidan/springboot/levelog/LevelogKafkaDefaultConsumerTest.java
+++ src/test/java/com/github/rozidan/springboot/levelog/LevelogKafkaDefaultConsumerTest.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2017 Idan Rozenfeld the original author or authors
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -34,7 +34,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
 @TestPropertySource(properties = {
         "levelog.kafka.servers:localhost:19092",
-        "levelog.kafka.enabled=true"})
+        "levelog.kafka.enabled=true",
+        "spring.application.name=appTest"})
 public class LevelogKafkaDefaultConsumerTest {
 
     @Autowired(required = false)
